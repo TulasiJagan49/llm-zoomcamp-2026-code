4c86a3eab31f327d641b889d25da66502a1c3217
TulasiJagan49 <jaganmeruga777gmail.com>
2026-06-17 11:52:01 +0530

    feat(rag): in the most naive way possible

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 from openai import OpenAI
 
+client = OpenAI(base_url="http://localhost:11434/v1", api_key="ollama")
+
+
+def llm(prompt):
+    response = client.responses.create(model="llama3.1", input=prompt)
+    return response.output_text
+
+
 def main():
     print("Hello from llm-zoomcamp-2026-code!")
 
-    client = OpenAI(
-        base_url="http://localhost:11434/v1",
-        api_key="ollama"
+    question = (
+        "Do you know about the ongoing football world cup? How many nations are in it??"
     )
 
-    response = client.chat.completions.create(
-        model="llama3.1",
-        messages=[
-            {"role": "system", "content": "You are a helpful coding assistant"},
-            {"role": "user", "content": "Write me a python function to reverse a string"},
-        ]
-    )
+    # Run this to see the answer provided by the llm
+    # before providing context about ongoing world cup
+    # answer = llm(question)
+    # print(answer)
 
-    print(response.choices[0].message.content)
+    # Now, we add context about the ongoing 2026 worldcup
+    context = """
+    Get the latest information from this website: 
+    https://www.fifa.com/en/tournaments/mens/worldcup/canadamexicousa2026,
+    and give me the answers to my queries regarding the tournament.
+    """
 
+    prompt = f"""
+    Context: {context}
+    Question: {question}
+
+    If you are not able to access the url, let me know.
+    Please do not make any assumptions about the information
+    not known to you.
+    """
+    print(llm(prompt))
 
 if __name__ == "__main__":
     main()
