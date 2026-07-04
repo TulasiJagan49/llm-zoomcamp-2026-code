08c04fb1653540b99c601807611812db9f6b602f
TulasiJagan49 <jaganmeruga777gmail.com>
2026-07-04 11:32:59 +0530

    chore(retrieval): index docs via minsearch and perform search

--- retrieval.py
+++ retrieval.py
@@ -1,5 +1,7 @@
 import requests
 
+from minsearch import Index
+
 '''
 The faqs data is cleaned and prepared by course owner, and provided to us,
 by the course owner of datatalk.clubs. So, that we can work on LLM related
@@ -23,6 +25,44 @@ for course in courses_list:
 
     faqs_data.extend(course_faqs)
 
-print(f"Total FAQs: {len(faqs_data)}")
+# print(f"Total FAQs: {len(faqs_data)}")
 
-print("Sample FAQ document:", faqs_data[0])
+# print("Sample FAQ document:", faqs_data[0])
+
+'''
+More information about the minsearch library can be found here:
+https://github.com/alexeygrigorev/build-your-own-search-engine
+'''
+index = Index(
+    text_fields=[
+        "question",
+        "section",
+        "answer"
+    ],
+    keyword_fields=[
+        "course"
+    ],
+)
+
+index.fit(faqs_data)
+
+
+def search(question, course):
+    boost_params = {
+        "question": 2.0,
+        "answer": 1.0,
+        "section": 0.5,
+    }
+    filter_params = {
+        "course": course,
+    }
+
+    return index.search(
+        question,
+        num_results=5,
+        boost_dict=boost_params,
+        filter_dict=filter_params,
+    )
+
+
+print(search(question="Can I enroll in the course after the start date?", course="llm-zoomcamp"))
